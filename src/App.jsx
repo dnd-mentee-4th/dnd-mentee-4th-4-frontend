4ba01dd68407e65b62ec77887b16b76211b8429e
jinsoo <admin@binary01.me>
2021-01-30 15:44:02 +0900

    feat: intro page route add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Intro from './components/Intro';
 import Main from './containers/main';
 
 const App = () => (
   <BrowserRouter basename="/">
     <Switch>
-      <Route path="/" component={Main} exact />
+      <Route path="/" component={Intro} exact />
+      <Route path="/main" component={Main} exact />
     </Switch>
   </BrowserRouter>
 );
