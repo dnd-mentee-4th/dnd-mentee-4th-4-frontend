29a5b4bd831472229113b8ba66f677efa6ac2297
phaesol <parkhaesol0227@likelion.org>
2021-02-05 19:40:25 +0900

    intro fix

--- src/components/Layout/Sidebar/Sidebar.jsx
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
+
 import React, { useContext } from 'react';
 import styled, { css } from 'styled-components';
 import { Collapse } from 'antd';
